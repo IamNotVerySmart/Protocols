bdd574dc9feab98afd29823079d01773ec6a71e0
Kacper Chrabąszcz <kacper.chrabaszcz@teb.edu.pl>
2024-03-20 22:23:07 +0100

    Add project files.

---
+++ Protocols/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Protocols.ChildWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Protocols
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StationCreate(object sender, RoutedEventArgs e)
+        {
+            SubWindow StationCreate = new SubWindow();
+            StationCreate.Owner = this;
+            StationCreate.Show();
+            /*Ellipse station = new Ellipse
+            {
+                Width = 20,
+                Height = 20,
+                Fill = Brushes.Red
+            };
+
+            Canvas.SetLeft(station, 100);
+            Canvas.SetTop(station, 100);
+
+            StationsArea.Children.Add(station)*/
+            ;
+        }
+    }
+}
